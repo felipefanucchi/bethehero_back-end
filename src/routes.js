adc406cd395c3b976f87e728c1ae5c922f1af391
Felipe Fanucchi <ffanucchi@studiovisual.com.br>
2020-03-24 22:44:49 -0300

    Criando rota do tipo POST para cadastrar ong no banco

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
+
 const router = express.Router();
 
-router.post('/users/:id', (request, response) => {
-  return response.json({
-    params: request.params.id,
-    query: {
-    ...request.query,
-    },
-    body: {
-    ...request.body
-    }
+router.post('/ongs', async (request, response) => {
+  const {name, email, whatsapp, city, uf} = request.body;
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
   });
+
+  return response.json({ id });
 });
 
 module.exports = router;
