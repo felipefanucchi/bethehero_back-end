265652ce134747b9767d5602ddab37040a291ac1
Felipe Fanucchi <ffanucchi@studiovisual.com.br>
2020-03-24 21:45:43 -0300

    Entendendo tipos de parametros e configurando nodemon

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({
-        evento: 'Semana omnistack 11.0',
-        aluno: 'Felipe Fanucchi'
-    });
+app.use(express.json());
+
+app.post('/users/:id', (request, response) => {
+  return response.json({
+    params: request.params.id,
+    query: {
+      ...request.query,
+    },
+    body: {
+      ...request.body
+    }
+  });
 })
 
 app.listen(3333);
