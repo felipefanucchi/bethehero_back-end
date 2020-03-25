a1e5f3ac36d9e8f74f1fe3961943386a0e3ec3bf
Felipe Fanucchi <ffanucchi@studiovisual.com.br>
2020-03-25 00:24:42 -0300

    Criando rota de login

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 // Controllers
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 router.get('/ongs', OngController.index);
 router.post('/ongs', OngController.create);
@@ -11,5 +13,8 @@ router.post('/ongs', OngController.create);
 router.post('/incidents', IncidentController.create);
 router.get('/incidents', IncidentController.index);
 router.delete('/incidents/:id', IncidentController.delete);
+
+router.get('/profile', ProfileController.index);
+router.post('/sessions', SessionController.create);
 
 module.exports = router;
