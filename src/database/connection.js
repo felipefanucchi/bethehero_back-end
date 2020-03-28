9aeccc928355b8cbc5254f564eb61899712b4e62
Felipe Fanucchi <ffanucchi@studiovisual.com.br>
2020-03-28 14:59:15 -0300

    Ajustando variaveis de ambiente

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
-
-const connection = knex(configuration);
+const { NODE_ENV } = process.env;
+const env = NODE_ENV || 'development';
+const connection = knex(configuration[env]);
 
 module.exports = connection;
