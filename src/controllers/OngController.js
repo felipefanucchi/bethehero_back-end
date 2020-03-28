e6cdd16a1f9e7c1a70f2a44893923a49f50a04d6
Felipe Fanucchi <ffanucchi@studiovisual.com.br>
2020-03-28 15:03:31 -0300

    Adiciona bloco try

--- src/controllers/OngController.js
+++ src/controllers/OngController.js
@@ -5,20 +5,25 @@ module.exports = {
   async create(request, response) {
     const {name, email, whatsapp, city, uf} = request.body;
     const id = crypto.randomBytes(4).toString('HEX');
-    
-    await connection('ongs').insert({
-      id,
-      name,
-      email,
-      whatsapp,
-      city,
-      uf,
-    })
-    .catch(err => (
-      response.status(400).json({
-        error: 'User already registered.',
+
+    try {
+      await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
       })
-    ));
+      .catch(err => (
+        response.status(400).json({
+          error: 'User already registered.',
+        })
+      ));
+    
+    } catch(error) {
+      alert('Erro na requisição, tente novamente mais tarde', error)
+    }
   
     return response.json({ id });
   },
