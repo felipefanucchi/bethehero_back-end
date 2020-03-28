1d0c4c95c3f8256ae9412d473c163d0fd8fa22b6
Felipe Fanucchi <ffanucchi@studiovisual.com.br>
2020-03-28 15:55:07 -0300

    wip

--- src/database/migrations/20200324221739_create_incidents.js
+++ src/database/migrations/20200324221739_create_incidents.js
@@ -5,7 +5,7 @@ exports.up = function(knex) {
 
     table.string('title').notNullable();
     table.string('description').notNullable();
-    table.string('value').notNullable();
+    table.decimal('value').notNullable();
 
     table.string('ong_id').notNullable();
     table.foreign('ong_id').references('id').inTable('ongs');
